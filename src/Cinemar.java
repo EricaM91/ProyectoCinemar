fcf1ef11a30ceecd6240de1130d463fe68a9ed00
EricaM91 <ericaarcaine@gmail.com>
2022-06-15 14:29:19 -0300

    Añadi enter

--- src/Cinemar.java
+++ src/Cinemar.java
@@ -4,7 +4,8 @@ public class Cinemar {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Este es el proyecto Cinemar");
+		System.out.println("Este es el proyecto Cinemar"); 
+		
 
 	}
 
