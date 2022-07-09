2e7e191e6bd14a7c05ee2adddf7d8828bf6b886e
CCM2022 <caritoarcaine@gmail.com>
2022-07-08 23:13:51 -0300

    Creando el menu para Usuario

--- src/Cinemar/AppMain.java
+++ src/Cinemar/AppMain.java
@@ -6,28 +6,18 @@ import java.util.Scanner;
 import Peliculas.Butacas;
 import Peliculas.Pelicula;
 import Peliculas.Sala;
+import Usuario.Usuario;
 
 
    public class AppMain {
 	 
    
-	
+   static Scanner leer = new Scanner(System.in);
    public static void main(String[] args) {
 	   
 	   //PAQUETE USUARIO
 		
-		Scanner leer = new Scanner(System.in);
 		
-		System.out.println("###### BIENVENIDOS A CINEMAR ######");
-		System.out.println("ELIGE UNA OPCION");
-		System.out.println("\n");
-		System.out.println("1. INICIAR SESION");
-		System.out.println("2. REGISTRARSE");
-		System.out.println("3. CREAR UNA RESERVA");
-		System.out.println("4. MODIFICAR UNA RESERVA");
-		System.out.println("5. OBSERVAR MIS RESERVAS");
-		System.out.println("6. VER HISTORIAL DE MIS ENTRADAS");
-		System.out.println("0. SALIR");
 		
 		
 		/*String email;
@@ -44,96 +34,182 @@ import Peliculas.Sala;
 		
 		//PAQUETE PELICULA
 
-		ArrayList <Pelicula> ListaPeliculas = new ArrayList <Pelicula> ();
-		
-		Pelicula pelicula0 = new Pelicula("Dog un viaje salvaje", "Drama", 101, "Briggs, un ex ranger del ejército que se aventurará a cruzar el país junto a su perra Lulu", "Español Latino" );
-		Pelicula pelicula1= new Pelicula("Jurassic world dominio", "Aventura", 147, "Los dinosaurios ahora viven y cazan junto a los humanos en todo el mundo. Este frágil equilibrio cambiará el futuro", "Español Latino");
-		Pelicula pelicula2= new Pelicula("Minions nace un villano", "Animacion", 87, "Llega la historia de un adolescente de 12 años que sueña con convertirse en el villano más grande del mundo: MINIONS: NACE UN VILLANO", "Español Latino");
-		Pelicula pelicula3= new Pelicula("Thor amor y trueno", "Aventura", 125, " Thor en una búsqueda por la paz interior, es interrumpido por un asesino galáctico conocido como Gorr,", "Español Latino");
-		Pelicula pelicula4= new Pelicula("Lightyear", "Animacion", 100, "Película sobre el joven piloto de pruebas Buzz Lightyear quién luego se convertiría en el popular juguete que todos los niños querrían tener en Toy Story" , "Español Latino");
-		Pelicula pelicula5= new Pelicula("El telefono negro", "Terror", 104, "Finney Shaw, un tímido, pero inteligente niño de 13 años es secuestrado por un sádico asesino y atrapado en un aterrador, oscuro y aislado sótano", "Ingles");
-		Pelicula pelicula6= new Pelicula("Todo en todas partes al mismo tiempo", "Accion", 139, "Una inmigrante china, se ve envuelta en una aventura salvaje en la que solo ella puede salvar el mundo explorando otros universos", "Ingles");
-		Pelicula pelicula7= new Pelicula("Top gun maverick", "Accion", 131, "Después de más de treinta años de servicio como uno de los mejores aviadores de la Armada, Pete está donde pertenece, como un valiente piloto", "Ingles");
-		Pelicula pelicula8= new Pelicula(null, null, 0, null, null);
-		Pelicula pelicula9= new Pelicula(null, null, 0, null, null);
-		Pelicula pelicula10= new Pelicula(null, null, 0, null, null);
-		
-		ListaPeliculas.add(pelicula0);
-		ListaPeliculas.add(pelicula1);
-		ListaPeliculas.add(pelicula2);
-		ListaPeliculas.add(pelicula3);
-		ListaPeliculas.add(pelicula4);
-		ListaPeliculas.add(pelicula5);
-		ListaPeliculas.add(pelicula6);
-		ListaPeliculas.add(pelicula7);
-		
-		ArrayList <Butacas> listaButacas = new ArrayList <Butacas> ();
-		Butacas oneButacas = new Butacas(1,1,true);
-		listaButacas.add(oneButacas);
-		Butacas twoButacas = new Butacas(1,2,false);
-		listaButacas.add(twoButacas);
-		Butacas threeButacas = new Butacas(1,3,true);
-		listaButacas.add(threeButacas);
-		Butacas fourButacas = new Butacas(1,4,true);
-		listaButacas.add(fourButacas);
-		Butacas fiveButacas = new Butacas(1,5,true);
-		listaButacas.add(fiveButacas);
-		Butacas sixButacas = new Butacas(1,6,false);
-		listaButacas.add(sixButacas);
-		Butacas sevenButacas = new Butacas(1,7,false);
-		listaButacas.add(sevenButacas);
-		Butacas eightButacas = new Butacas(1,8,true);
-		listaButacas.add(eightButacas);
-		Butacas nineButacas = new Butacas(1,9,true);
-		listaButacas.add(nineButacas);
-		Butacas tenButacas = new Butacas(1,10,true);
-		listaButacas.add(tenButacas);
-		//Agregar ciclo?
-		
-		//Salas
-		
-		
-	    Sala sala1 = new Sala(1, "2D", listaButacas );
-	    Sala sala2 = new Sala(2, "3D", listaButacas ); 
-	    Sala sala3 = new Sala(3, "2D", listaButacas );
-	    Sala sala4 = new Sala(4, "3D", listaButacas );
-	    Sala sala5 = new Sala(5, "2D", listaButacas );
-	    Sala sala6 = new Sala(6, "3D", listaButacas );
-	    Sala sala7 = new Sala(7, "2D", listaButacas );
-	    Sala sala8 = new Sala(8, "3D", listaButacas );
-	    Sala sala9 = new Sala(9, "2D", listaButacas );
-	    Sala sala10 = new Sala(10, "3D", listaButacas );
+//		ArrayList <Pelicula> ListaPeliculas = new ArrayList <Pelicula> ();
+//		
+//		Pelicula pelicula0 = new Pelicula("Dog un viaje salvaje", "Drama", 101, "Briggs, un ex ranger del ejï¿½rcito que se aventurarï¿½ a cruzar el paï¿½s junto a su perra Lulu", "Espaï¿½ol Latino" );
+//		Pelicula pelicula1= new Pelicula("Jurassic world dominio", "Aventura", 147, "Los dinosaurios ahora viven y cazan junto a los humanos en todo el mundo. Este frï¿½gil equilibrio cambiarï¿½ el futuro", "Espaï¿½ol Latino");
+//		Pelicula pelicula2= new Pelicula("Minions nace un villano", "Animacion", 87, "Llega la historia de un adolescente de 12 aï¿½os que sueï¿½a con convertirse en el villano mï¿½s grande del mundo: MINIONS: NACE UN VILLANO", "Espaï¿½ol Latino");
+//		Pelicula pelicula3= new Pelicula("Thor amor y trueno", "Aventura", 125, " Thor en una bï¿½squeda por la paz interior, es interrumpido por un asesino galï¿½ctico conocido como Gorr,", "Espaï¿½ol Latino");
+//		Pelicula pelicula4= new Pelicula("Lightyear", "Animacion", 100, "Pelï¿½cula sobre el joven piloto de pruebas Buzz Lightyear quiï¿½n luego se convertirï¿½a en el popular juguete que todos los niï¿½os querrï¿½an tener en Toy Story" , "Espaï¿½ol Latino");
+//		Pelicula pelicula5= new Pelicula("El telefono negro", "Terror", 104, "Finney Shaw, un tï¿½mido, pero inteligente niï¿½o de 13 aï¿½os es secuestrado por un sï¿½dico asesino y atrapado en un aterrador, oscuro y aislado sï¿½tano", "Ingles");
+//		Pelicula pelicula6= new Pelicula("Todo en todas partes al mismo tiempo", "Accion", 139, "Una inmigrante china, se ve envuelta en una aventura salvaje en la que solo ella puede salvar el mundo explorando otros universos", "Ingles");
+//		Pelicula pelicula7= new Pelicula("Top gun maverick", "Accion", 131, "Despuï¿½s de mï¿½s de treinta aï¿½os de servicio como uno de los mejores aviadores de la Armada, Pete estï¿½ donde pertenece, como un valiente piloto", "Ingles");
+//		Pelicula pelicula8= new Pelicula(null, null, 0, null, null);
+//		Pelicula pelicula9= new Pelicula(null, null, 0, null, null);
+//		Pelicula pelicula10= new Pelicula(null, null, 0, null, null);
+//		
+//		ListaPeliculas.add(pelicula0);
+//		ListaPeliculas.add(pelicula1);
+//		ListaPeliculas.add(pelicula2);
+//		ListaPeliculas.add(pelicula3);
+//		ListaPeliculas.add(pelicula4);
+//		ListaPeliculas.add(pelicula5);
+//		ListaPeliculas.add(pelicula6);
+//		ListaPeliculas.add(pelicula7);
+//		
+//		ArrayList <Butacas> listaButacas = new ArrayList <Butacas> ();
+//		Butacas oneButacas = new Butacas(1,1,true);
+//		listaButacas.add(oneButacas);
+//		Butacas twoButacas = new Butacas(1,2,false);
+//		listaButacas.add(twoButacas);
+//		Butacas threeButacas = new Butacas(1,3,true);
+//		listaButacas.add(threeButacas);
+//		Butacas fourButacas = new Butacas(1,4,true);
+//		listaButacas.add(fourButacas);
+//		Butacas fiveButacas = new Butacas(1,5,true);
+//		listaButacas.add(fiveButacas);
+//		Butacas sixButacas = new Butacas(1,6,false);
+//		listaButacas.add(sixButacas);
+//		Butacas sevenButacas = new Butacas(1,7,false);
+//		listaButacas.add(sevenButacas);
+//		Butacas eightButacas = new Butacas(1,8,true);
+//		listaButacas.add(eightButacas);
+//		Butacas nineButacas = new Butacas(1,9,true);
+//		listaButacas.add(nineButacas);
+//		Butacas tenButacas = new Butacas(1,10,true);
+//		listaButacas.add(tenButacas);
+//		//Agregar ciclo?
+//		
+//		//Salas
+//		
+//		
+//	    Sala sala1 = new Sala(1, "2D", listaButacas );
+//	    Sala sala2 = new Sala(2, "3D", listaButacas ); 
+//	    Sala sala3 = new Sala(3, "2D", listaButacas );
+//	    Sala sala4 = new Sala(4, "3D", listaButacas );
+//	    Sala sala5 = new Sala(5, "2D", listaButacas );
+//	    Sala sala6 = new Sala(6, "3D", listaButacas );
+//	    Sala sala7 = new Sala(7, "2D", listaButacas );
+//	    Sala sala8 = new Sala(8, "3D", listaButacas );
+//	    Sala sala9 = new Sala(9, "2D", listaButacas );
+//	    Sala sala10 = new Sala(10, "3D", listaButacas );
+//
+//
+//	    sala1.mostrar();
+//	    sala2.mostrar();
+//	    sala3.mostrar();
+//	    sala4.mostrar();
+//	    sala5.mostrar();
+//	    sala6.mostrar();
+//	    sala7.mostrar();
+//	    sala8.mostrar();
+//	    sala9.mostrar();
+//	    sala10.mostrar();
+//	    
+//
+//	    Scanner lectura = new Scanner(System.in);
+//	    
+//	    System.out.println("\n#####################   MENU CINEMAR PARA LA ADMINISTRACION   #####################\n");
+//	    System.out.println("#####################   SELECCIONE UNA OPCION   ######################");
+//	    System.out.println("\n");
+//	    System.out.println("1-VER TODAS LAS RESERVAS");
+//	    System.out.println("2-VER UN CLIENTE EN PARTICULAR");
+//	    System.out.println("3-CREAR SALA CON UNA PELICULA");
+//	    System.out.println("4-MODIFICAR LA SALA");
+//	    System.out.println("5-ELIMINAR LA SALA");
+//	    System.out.println("6-MODIFICAR DESCUENTOS");
+//	    
+	   int option  = MenuClient();
+	   switch (option) {
+	   case 1:
+		   Scanner leerScanner=new Scanner(System.in);
+			System.out.println("Cinemar");
+			System.out.println("Iniciar sesion");
+			
+			System.out.print("Email:");
+			String email = leerScanner.nextLine();
+			System.out.println();
 
-
-	    sala1.mostrar();
-	    sala2.mostrar();
-	    sala3.mostrar();
-	    sala4.mostrar();
-	    sala5.mostrar();
-	    sala6.mostrar();
-	    sala7.mostrar();
-	    sala8.mostrar();
-	    sala9.mostrar();
-	    sala10.mostrar();
+			System.out.print("Clave:");
+			String password=leerScanner.nextLine();
+			
+			System.out.println();
+			Usuario usuario = new Usuario(email, password);
+			
+			//Usuario u = login.loguearme();
+	     break;
+	   case 2:
+	     System.out.println("Tuesday");
+	     break;
+	   case 3:
+	     System.out.println("Wednesday");
+	     break;
+	   case 4:
+	     System.out.println("Thursday");
+	     break;
+	   case 5:
+	     System.out.println("Friday");
+	     break;
+	   case 6:
+	     System.out.println("Saturday");
+	     break;
+	   case 0:
 	    
-
-	    Scanner lectura = new Scanner(System.in);
-	    
-	    System.out.println("\n#####################   MENU CINEMAR PARA LA ADMINISTRACION   #####################\n");
-	    System.out.println("#####################   SELECCIONE UNA OPCION   ######################");
-	    System.out.println("\n");
-	    System.out.println("1-VER TODAS LAS RESERVAS");
-	    System.out.println("2-VER UN CLIENTE EN PARTICULAR");
-	    System.out.println("3-CREAR SALA CON UNA PELICULA");
-	    System.out.println("4-MODIFICAR LA SALA");
-	    System.out.println("5-ELIMINAR LA SALA");
-	    System.out.println("6-MODIFICAR DESCUENTOS");
-	    
-	    
-
+	     break;
+	 }
 	
 }
    
-   
+	public static int MenuClient() {
+		
+		String result;
+		int option = 0;
+		boolean bandera = false;
+		do{
+			CleanScreen();
+			System.out.println("###### BIENVENIDOS A CINEMAR ######");
+			System.out.println("ELIGE UNA OPCION");
+			System.out.println("\n");
+			System.out.println("1. INICIAR SESION");
+			System.out.println("2. REGISTRARSE");
+			System.out.println("3. CREAR UNA RESERVA");
+			System.out.println("4. MODIFICAR UNA RESERVA");
+			System.out.println("5. OBSERVAR MIS RESERVAS");
+			System.out.println("6. VER HISTORIAL DE MIS ENTRADAS");
+			System.out.println("0. SALIR");
+			System.out.println("\n");
+			
+			result = leer.nextLine();
+		    try {
+
+		    	option = Integer.parseInt(result);
+		    	if(option>=0 && option<=6)
+		    	{
+		    		bandera = false;
+		    	}
+		    	else if(option < 0 || option > 6) 
+				{
+					bandera = true;
+					System.out.println("Ingrese una opcion Valida...");
+				} 
+			   
+	        }
+	        catch (Exception e)
+		    {
+	        	bandera = true;
+	        	System.out.println("Ingrese una opcion Valida...");
+	        } 
+		
+		}while(bandera);
+		return option;
+		
+		
+	}
+	
+	public static void CleanScreen() {
+		 System.out.print("\033[H\033[2J");
+	        System.out.flush();
+	}
+	    
 
 }
